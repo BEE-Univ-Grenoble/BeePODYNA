77511b3bfbffc3f79115be0567bd13e655857ab0
BartaNico <bartalucci.nico@gmail.com>
2019-09-30 14:44:18 +0200

    update names function

--- R/attributes.R
+++ R/attributes.R
@@ -1,3 +1,18 @@
-names.population <- function(pop) {
-  pop$label
+#' Returns the name of the population
+#'
+#' @param pop is an object of the class 'population'
+#'
+#' @return returns the name of the population.
+#'
+#' @examples
+#'     pop=population("test",42,2,200)
+#'     names.population(pop)
+#'
+#' @author
+#'
+#' @export
+
+names.population = function(pop) {
+  if is.population(pop)==T {pop$label}
+  else {stop("The object must be a population.")}
 }
