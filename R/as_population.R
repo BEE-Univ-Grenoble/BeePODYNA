72f9b22e5abb7ea7f35c8c8fb05669a98a5eee95
Eric Coissac <eric.coissac@metabarcoding.org>
2019-11-26 10:04:14 +0100

    refactoring

--- R/as_population.R
+++ R/as_population.R
@@ -19,7 +19,7 @@
 #' obj = list(label ="pop.test",size= 100,growth_rate = 0.1,capacity = 500)
 #' as_population(obj)
 #'
-#'
+#' @author Martial
 #' @export
 
 as_population = function(object){
@@ -95,61 +95,4 @@ as_population.list <- function(object) {
    newpop
 
 }
-
-# # creation of a new class, type : List, containing characters and numeric.
-#
-#   setClass("population",representation("list"),contains = c("character","numeric"))
-#
-# # Check list element
-#   if(class(object) == "list"){
-#       population(object)
-#
-#     }
-#
-#   }
-#
-# # If object is a data frame
-#
-#   df_object = as.data.frame(object)
-#   colnames(df_object) = c("time","pop") # /!\ DF must be in the right order
-#
-# ## Find the intial population :
-#
-#   init = df_object$pop[which(df.object$time == 0)]
-#
-# ## Find the growth rate
-#
-#   ### (too?) simple way :
-#   gr.tempo = numeric(length(df_object$time))
-#
-#   for (i in c(1:length(df_object$time)-1)) {
-#
-#     gr_tempo[i] = (df_object$pop[i+1]-df_object$pop[i])/(df_object$time[i+1]-df_object$time[i])
-#
-#   }
-# # GR : Growth Rate
-#   approx_GR = mean(gr_tempo)
-#
-#   df_GR = as.data.frame(cbind(df_object$time,df_object$pop,gr_tempo)) #make a DF with the time and the growth rate
-#   colnames(df_GR) = c("time","pop","growth_rate")
-#
-#   ### maybe try to fit a model on the data and extract the GR,
-#
-# ## Find the limit capacity : (where pop is stable ie. GR = 0)
-#
-#   for (i in length(df_GR$time)) {
-#
-#     if(sum(df_GR$growth_rates[i],df_GR$growth_rates[i+1]) == 0 ){
-#
-#       K = df_GR$pop[i]
-#     }
-#
-#   }
-#
-# # @example
-#
-#   }
-
-
-
 
