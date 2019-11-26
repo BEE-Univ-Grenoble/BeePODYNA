8efad94d38b767444602e47787bff8fdf9df28e1
martialeroy <44521950+martialeroy@users.noreply.github.com>
2019-11-26 16:30:55 +0100

    ajout d'une comunauté et creation de deux objet beepodyna, beepod_hudson et beebop_sweden,

--- data_prep.R
+++ data_prep.R
@@ -2,9 +2,9 @@
 # Hudson data preparation
 library(BeePODYNA)
 #file at : https://github.com/stan-dev/example-models/blob/master/knitr/lotka-volterra/hudson-bay-lynx-hare.csv
-Hudson <-
-  read.csv("raw_data/hudson-bay-lynx-hare.csv",
+Hudson <- read.csv("raw_data/hudson-bay-lynx-hare.csv",
            comment.char="#")
+
 
 lynx = population("lynx",4,1,60)
 hare = population("hare",30,2,80)
@@ -14,6 +14,33 @@ lynx[[3]] = hare[[3]] = Hudson$Year
 hudson = community('Hudson',hare,lynx)
 
 usethis::use_data(hudson, overwrite = TRUE)
+
+# deuxieme communauté :
+# further readings https://www.nature.com/scitable/knowledge/library/dynamics-of-predation-13229468/
+
+red_fox = population("red_fox",10,1.2,50)
+vole = population("vole",100,2,200)
+gouse = population("Gouse",30,1.5,80)
+Sweden = community('Sweden',red_fox,vole,gouse)
+
+usethis::use_data(Sweden, overwrite = TRUE)
+
+
+beepod_sweden = beepodyna(label="beepod_sweden",
+                     community = Sweden,
+                   interactions=interactions(3),
+                     c(exponential_growth,exponential_growth,exponential_growth),
+                   verbose = FALSE)
+usethis::use_data(beepod_sweden, overwrite = TRUE)
+
+beepod_hudson = beepodyna(label="beepod_hudson",
+                          community = hudson,
+                          interactions=interactions(2),
+                          c(exponential_growth,exponential_growth),
+                          verbose = FALSE)
+
+usethis::use_data(beepod_hudson, overwrite = TRUE)
+
 #data("hudson")
 #plot(hudson)
 
