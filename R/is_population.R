658400b2d44f766b9d78742e5326e2217f4ad6a8
Eric Coissac <eric.coissac@metabarcoding.org>
2019-11-26 10:43:36 +0100

    Refactoring code and doc

--- R/is_population.R
+++ R/is_population.R
@@ -1,16 +1,21 @@
-#' is_population
+#' Tests if an object is belonging the class \code{population}.
 #'
 #' The function determinines if the object is a population or not
+#'
 #' @param x the object which must be a population to validate the condition
+#'
 #' @return a logical "TRUE" or "FALSE"
+#'
+#' @examples
+#'
+#'   p <- population('example', 2, 0.5, 10)
+#'   is_population(p) # Returns TRUE
+#'   is_population(4) # Returns FALSE
+#'
+#'
 #' @author Cresciense Lecaude
 #'
 #' @export
 is_population <- function(x){
-  if (class(x) == "population"){
-    return(TRUE)
-  }
- else {
-  return(FALSE)
-  }
+  class(x) == "population"
 }
