7d2b40a75aaa4dd227e7f3850c069c34cd4392d7
BartaNico <bartalucci.nico@gmail.com>
2019-11-26 15:36:05 +0100

    Add logistic function and improve the documentation

--- R/simulate.R
+++ R/simulate.R
@@ -29,16 +29,15 @@ simulate_onestep <- function(beepodyna) {
 
 #' Function describing an exponential growth, has to bee included in a beepodyda object when creating it
 #'
-#' @param pop Index of the population that will be affected by the function
-#' @param community a community object created with community()
-#' @param interactions an interactions object created with interactions()
+#' @param pop index of the population that will be affected by the function, or the name of the pop
+#' @param community a `community` object created with community()
+#' @param interactions an `interactions` object created with interactions()
 #'
-#' @return The function return a community similare to the one given in `comunity` but the population selected with `pop` will have one more time index and size (calculed with its growth rate, according to an exponential growth)
+#' @return The function returns a community similare to the one given in `community` but the population selected with `pop` will have one more time index and size (calculed with its growth rate, according to an exponential growth)
 #'
 #' @examples
 #'     data(hudson)
 #'     exponential_growth(1,hudson,interactions(2))
-#'
 #'
 #' @export
 exponential_growth <- function(pop, community, interactions) {
@@ -57,7 +56,7 @@ exponential_growth <- function(pop, community, interactions) {
   community
 }
 
-#'Compute  n times steps of evolution of the populations contained in a beepodyna object
+#'Compute n times steps of evolution of the populations contained in a beepodyna object
 #'
 #'@param beepodyna an object of class beepodyna
 #'@param n The number of times steps to be computed
@@ -80,4 +79,33 @@ simulate_n_pop_dynamic <- function(beepodyna,n) {
     beepodyna <- simulate_onestep(beepodyna)
 
   beepodyna
+}
+
+#' Function describing a logistical growth, has to bee included in a beepodyda object when creating it
+#'
+#' @param pop index of the population that will be affected by the function, or the name of the pop
+#' @param community a `community` object created with community()
+#' @param interactions an `interactions` object created with interactions()
+#'
+#' @return The function returns a community similare to the one given in `community` but the population selected with `pop` will have one more time index and size (calculed with its growth rate and capacity, according to a logistical growth)
+#'
+#' @examples
+#'     data(hudson)
+#'     exponential_growth(1,hudson,interactions(2))
+#'
+#' @export
+logistic_growth <- function(pop, community, interactions) {
+  target_pop <- community$populations[[pop]]
+  last_time  <- target_pop$time[length(target_pop$time)]
+  last_size  <- target_pop$size[length(target_pop$size)]
+
+  new_size <- last_size + last_size * target_pop$growth_rate * (1 - last_size / target_pop$capacity)
+  new_time <- last_time + 1
+
+  target_pop$size <- append(target_pop$size, new_size)
+  target_pop$time <- append(target_pop$time, new_time)
+
+  community$populations[[pop]] <- target_pop
+
+  community
 }
